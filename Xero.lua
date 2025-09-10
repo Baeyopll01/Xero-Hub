534f13ad9d70ca36a9cd9644087c9cd2cc713550
Tobi Dev <163174419+Baeyopll01@users.noreply.github.com>
2025-09-10 13:53:43 +0700

    Add files via upload

--- Xero.lua
+++ Xero.lua
@@ -1201,54 +1201,107 @@ do
 	end
 end
 function p:Window(_)
-    local k = { Active = nil, Maximized = false, Visible = true, Ready = true } -- Ready = true ทันที
-    local _ = _ or {}
-    local m = {
-        StartupName = _.StartupName or _.startname or "Xero Hub",
-        Title = _.title or _.Title or "Window",
-        SubTitle = _.subtitle or _.Subtitle or _.SubTitle or "Xero Hub",
-        TabWidth = _.tabwidth or _.TabWidth or 160,
-        Size = _.size or _.Size or self.IsMobile and UDim2.fromOffset(464, 368) or UDim2.fromOffset(580, 460),
-        MinimizeKey = _.minimizekey or _.MinimizeKey or Enum.KeyCode.Home,
-        Theme = _.theme or _.Theme or "Default",
-        ExitCallback = _.exitcallback or _.ExitCallback or function() end,
-        Transparency = _.transparency or _.Transparency or 0.85,
-        FontFace = _.font or _.Font or nil,
-    }
-
-    p.Settings.FontFace = m.FontFace
-    self.Settings.Keybind = m.MinimizeKey
-    self.Settings.Transparency = m.Transparency
-    self.Theme = m.Theme
-
-    local _ = c:IsStudio() and a.PlayerGui or game:GetService("CoreGui")
-
-    local j = self:Create("ScreenGui", {
-        Name = "NEXT_GEN",
-        Parent = _,
-        ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
-        ResetOnSpawn = false,
-        DisplayOrder = 100000000000,
-        IgnoreGuiInset = true,
-    })
-
-    local _ = p:Create("Frame", {
-        Parent = j,
-        AnchorPoint = Vector2.new(0.5, 0.5),
-        BackgroundColor3 = Color3.new(1, 1, 1),
-        BackgroundTransparency = 1,
-        BorderColor3 = Color3.new(0, 0, 0),
-        BorderSizePixel = 0,
-        Position = UDim2.new(0.5, 0, 0.5, 0),
-        Size = UDim2.new(0.151, 0, 0.093, 0),
-    })
-    function p:Destroy()
-        for _, _ in pairs(self.Connections) do
-            _:Disconnect()
-        end
-        j:Destroy()
-    end
-end
+	local k = { Active = nil, Maximized = false, Visible = true, Ready = false }
+	local _ = _ or {}
+	local m = {
+		StartupName = _.StartupName or _.startname or "",
+		Title = _.title or _.Title or "Window",
+		SubTitle = _.subtitle or _.Subtitle or _.SubTitle or "",
+		TabWidth = _.tabwidth or _.TabWidth or 160,
+		Size = _.size or _.Size or self.IsMobile and UDim2.fromOffset(464, 368) or UDim2.fromOffset(580, 460),
+		MinimizeKey = _.minimizekey or _.MinimizeKey or Enum.KeyCode.Home,
+		Theme = _.theme or _.Theme or "Default",
+		ExitCallback = _.exitcallback or _.ExitCallback or function() end,
+		Transparency = _.transparency or _.Transparency or 0.85,
+		FontFace = _.font or _.Font or nil,
+	}
+	p.Settings.FontFace = m.FontFace
+	self.Settings.Keybind = m.MinimizeKey
+	self.Settings.Transparency = m.Transparency
+	self.Theme = m.Theme
+	local _ = c:IsStudio() and a.PlayerGui or game:GetService("CoreGui")
+	local j = self:Create("ScreenGui", {
+		Name = "NEXT_GEN",
+		Parent = _,
+		ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
+		ResetOnSpawn = false,
+		DisplayOrder = 100000000000,
+		IgnoreGuiInset = true,
+	})
+	local _ = p:Create("Frame", {
+		Parent = j,
+		AnchorPoint = Vector2.new(0.5, 0.5),
+		BackgroundColor3 = Color3.new(1, 1, 1),
+		BackgroundTransparency = 1,
+		BorderColor3 = Color3.new(0, 0, 0),
+		BorderSizePixel = 0,
+		Position = UDim2.new(0.5, 0, 0.5, 0),
+		Size = UDim2.new(0.151, 0, 0.093, 0),
+	})
+	local a = p:Create("TextLabel", {
+		Name = "StartupLabel",
+		Parent = _,
+		AnchorPoint = Vector2.new(0.5, 0.5),
+		BackgroundColor3 = Color3.new(1, 1, 1),
+		BackgroundTransparency = 1,
+		BorderColor3 = Color3.new(0, 0, 0),
+		BorderSizePixel = 0,
+		Position = UDim2.new(0.5, 0, 0.5, 0),
+		Size = UDim2.new(1, 0, 0.234, 0),
+		ZIndex = 2,
+		Font = Enum.Font.GothamBold,
+		Text = "",
+		TextColor3 = Color3.new(1, 1, 1),
+		TextScaled = true,
+		TextSize = 14,
+		TextWrapped = true,
+	})
+	local b = p:Create("TextLabel", {
+		Name = "InitializingLabel",
+		AnchorPoint = Vector2.new(0.5, 0.5),
+		BackgroundColor3 = Color3.new(1, 1, 1),
+		BackgroundTransparency = 1,
+		BorderColor3 = Color3.new(0, 0, 0),
+		BorderSizePixel = 0,
+		Position = UDim2.new(0.5, 0, 0.667, 0),
+		Size = UDim2.new(1, 0, 0.222, 0),
+		ZIndex = 2,
+		Font = Enum.Font.GothamBold,
+		Text = "",
+		TextColor3 = Color3.new(1, 1, 1),
+		TextScaled = true,
+		TextSize = 14,
+		TextWrapped = true,
+		TextTransparency = 1,
+	})
+	task.delay(0.01, function()
+		b.Parent = _
+		o:Animation(a, { Position = UDim2.new(0.5, 0, 0.345, 0) }, 0.1, Enum.EasingStyle.Linear)
+		o:Animation(b, { TextTransparency = 0 }, 0.1, Enum.EasingStyle.Linear)
+		task.wait(0.01)
+		o:Animation(a, { Position = UDim2.new(0.5, 0, 0.275, 0), TextTransparency = 1 }, 0.1, Enum.EasingStyle.Linear)
+		o:Animation(b, { Position = UDim2.new(0.5, 0, 0.5, 0) }, 0.1, Enum.EasingStyle.Linear)
+		task.wait(0.01)
+		local _ = o:Animation(b, { TextTransparency = 1 }, 0.1, Enum.EasingStyle.Linear)
+		_.Completed:Connect(function()
+			k.Ready = true
+			if a then
+				a:Destroy()
+			end
+			if b then
+				b:Destroy()
+			end
+		end)
+	end)
+	repeat
+		task.wait()
+	until k.Ready
+	function p:Destroy()
+		for _, _ in pairs(self.Connections) do
+			_:Disconnect()
+		end
+		j:Destroy()
+	end
 	local n = self:Create("Frame", {
 		Name = "MainFrame",
 		Parent = j,
