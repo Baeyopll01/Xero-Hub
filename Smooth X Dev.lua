dfb9c234963bbfa8ee74bc5941f9a696b7885023
Tobi Dev <163174419+Baeyopll01@users.noreply.github.com>
2025-09-23 20:35:37 +0700

    Add files via upload

---
+++ Smooth X Dev.lua
@@ -0,0 +1,13 @@
+
+Key = script_key
+
+if game.GameId == 8316902627 then
+  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Baeyopll01/Xero-Hub/refs/heads/Secret4869/Plant%20vs%20brainrot.lua"))()
+
+end
+if game.GameId == 8066283370 then
+  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Baeyopll01/Xero-Hub/refs/heads/Secret4869/Build%20a%20Zoo.lua"))()
+
+end
